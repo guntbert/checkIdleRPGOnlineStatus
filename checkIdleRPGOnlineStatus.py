4f04047f97d885334c8505cb3182105f8dd1e624
Guntbert Reiter <guntbert@gmx.at>
2021-06-07 21:28:52 +0200

    Initial commit

---
+++ checkIdleRPGOnlineStatus.py
@@ -0,0 +1,17 @@
+# check my online status for the IdleRPG on libera.chat
+import requests
+from bs4 import BeautifulSoup
+
+def get_xml():
+    url = 'https://idlerpg.lolhosting.net/xml.php?player=heisenbug'
+    response = requests.get(url)
+    return response
+
+def run_check():
+    soup = BeautifulSoup(get_xml())
+    soup.prettify()
+if __name__ == __main__:
+    run_check()
+
+
+
