ccb395c487f1a8b9a5c379ef1439274a51a148ee
Guntbert Reiter <guntbert@gmx.at>
2021-06-07 22:25:23 +0200

    Make the name a variable

--- checkIdleRPGOnlineStatus.py
+++ checkIdleRPGOnlineStatus.py
@@ -2,21 +2,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_xml():
-    url = 'https://idlerpg.lolhosting.net/xml.php?player=heisenbug'
+name = 'heisenbug'
+
+def get_xml(name):
+    url = f'https://idlerpg.lolhosting.net/xml.php?player={name}'
     response = requests.get(url)
     return response.text
 
-def run_check():
-    name = 'heisenbug'
-    soup = BeautifulSoup(get_xml(), 'xml')
+def run_check(name):
+    soup = BeautifulSoup(get_xml(name), 'xml')
     status = soup.find('online').get_text()
     if status == '1':
         print(f'All is well. {name} is online')
     else:
         print(f'ALERT! {name} is OFFLINE!')
 if __name__ == '__main__':
-    run_check()
+    run_check(name)
 
 
 
